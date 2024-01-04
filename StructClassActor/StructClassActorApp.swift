6bdc8aa52572a48e2070a1757653042c8935ad8b
Elaidzha1940 <eli080293@gmail.com>
2024-01-04 21:48:31 +0300

    StructClassActor

---
+++ StructClassActor/StructClassActorApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: StructClassActor
+//  File: StructClassActorApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 04.01.2024
+//
+//  */
+
+import SwiftUI
+
+@main
+struct StructClassActorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
